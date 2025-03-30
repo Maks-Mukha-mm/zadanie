7066c3ce5ef26af756810628e46d4dbd7b978dc2
Maks-Mukha-mm <s30049@pjwstk.edu.pl>
2025-03-30 17:50:43 +0200

    1

---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        private readonly IClientRepository _clientRepository;
+        private readonly IUserCreditService _userCreditService;
+
+        public UserService(IClientRepository clientRepository, IUserCreditService userCreditService)
+        {
+            _clientRepository = clientRepository;
+            _userCreditService = userCreditService;
+        }
+
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+                return false;
+
+            if (!email.Contains("@") || !email.Contains("."))
+                return false;
+
+            int age = CalculateAge(dateOfBirth);
+            if (age < 21)
+                return false;
+
+            var client = _clientRepository.GetById(clientId);
+
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            AssignCreditLimit(user, client.Type);
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+                return false;
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private int CalculateAge(DateTime birthDate)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - birthDate.Year;
+            if (now.Month < birthDate.Month || (now.Month == birthDate.Month && now.Day < birthDate.Day))
+                age--;
+
+            return age;
+        }
+
+        private void AssignCreditLimit(User user, string clientType)
+        {
+            if (clientType == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else if (clientType == "ImportantClient")
+            {
+                int creditLimit = _userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                user.CreditLimit = creditLimit * 2;
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                user.CreditLimit = _userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+            }
+        }
+    }
+}
---
+++ LegacyAppConsumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using LegacyApp;
+using System;
+
+namespace LegacyAppConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * DO NOT CHANGE THIS FILE AT ALL
+             */
+            IClientRepository clientRepository = new ClientRepository();
+            IUserCreditService userCreditService = new UserCreditService(); 
+            var userService = new UserService(clientRepository,userCreditService);
+            var addResult = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+            if (addResult)
+                Console.WriteLine($"Adding John Doe was successful");
+            else
+                Console.WriteLine($"Adding John Doe was unsuccessful");
+        }
+    }
+}
